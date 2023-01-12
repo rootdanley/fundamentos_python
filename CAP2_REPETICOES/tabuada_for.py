31e077471ed94d738cb0f758dfa7dab42db6e3b9
rootdanley <danleyalves@hotmail.com>
2023-01-11 23:39:27 -0300

    primeiro commit

---
+++ CAP2_REPETICOES/tabuada_for.py
@@ -0,0 +1,3 @@
+numero = int(input('Digite um numero: '))
+for tabuada in range(1,10+1,1):
+    print(f'{numero} x {tabuada} = {numero * tabuada}')
---
+++ CAP2_VARIAVEIS/variaveis.py
@@ -0,0 +1,18 @@
+nome = 'Humbero Delgado'
+empresa = 'FIAP'
+qtde_funcionarios = 580
+media_mensalidade = 856.50
+print(nome + ' Trabalha na empresa ' + empresa)
+print('Possui: ', qtde_funcionarios, 'funcionarios.')
+print('A media da mensalidade e de: ' + str(media_mensalidade))
+
+
+
+#usando input para receber valores
+nome = input('Digite o nome: ')
+empresa = input('Digite o nome da empresa: ')
+qtde_funcionarios = int(input('Digite qtd de funcionarios'))
+media_mensalidade = float(input('Digite a media da mensaliade: '))
+print(nome + ' Trabalha na empresa ' + empresa)
+print('Possui: ', qtde_funcionarios, 'funcionarios.')
+print('A media da mensalidade e de: ' + str(media_mensalidade))
