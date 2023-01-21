d9949f51f3a0f67f7b622b32907217d8cd0ac4a5
rootdanley <danleyalves@hotmail.com>
2023-01-21 14:38:23 -0300

    refatorando

--- CAP2_DECISOES/decisoes_simples.py
+++
@@ -1,8 +0,0 @@
-nome=input("Digite o nome: ")
-idade=int(input("Digite a idade: "))
-prioridade="NÃO"
-if idade>=65:
-    prioridade="SIM"
-print("O paciente " + nome + " possui atendimento prioritário? " + prioridade)
-
-
--- CAP2_REPETICOES/tabuada_for.py
+++
@@ -1,3 +0,0 @@
-numero = int(input('Digite um numero: '))
-for tabuada in range(1,10+1,1):
-    print(f'{numero} x {tabuada} = {numero * tabuada}')
